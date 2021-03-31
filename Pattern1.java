5722cb66442f5503ace5cdcd62947c080590bf2a
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-03-31 16:40:28 +0530

    Creating First Pattern using Stars
    *
    * *
    * * *
    * * * *
    * * * * *

---
+++ Pattern1.java
@@ -0,0 +1,29 @@
+//
+//*
+//* *
+//* * *
+//* * * *
+//* * * * *
+
+import java.util.*;
+
+public class Pattern1 
+{
+
+	public static void main(String[] args) 
+	{
+
+		int n=5;
+		
+		for(int j=1;j<n+1;j++)
+		{
+		for (int i=0;i<j;i++)
+		{
+
+			System.out.print("* ");
+		}
+		System.out.println();
+		}
+	}
+
+}
